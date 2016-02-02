b29d4347a3f954ea9e30a8d37f019a8b1a7eae2e
Adam Marshall <adam@appsynergy.net>
2016-02-02 12:54:24 +0000

    Add assets and boilerplate

---
+++ Gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var elm  = require('gulp-elm');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch')
+
+swallowError = function(error) {
+  console.log(error.toString());
+  this.emit('end');
+}
+
+gulp.task('default', ['elm', 'index']);
+
+
+gulp.task('watch', function() {
+  gulp.watch('src/**', ['elm']);
+});
+
+
+gulp.task('elm', function() {
+  elm.init();
+  return gulp.src('src/Tiled.elm')
+    .pipe(plumber())
+    .pipe(elm())
+    .pipe(uglify())
+    .on('error', swallowError)
+    .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task('index', function() {
+  return gulp.src('assets/index.html')
+    .pipe(gulp.dest('build/'));
+});
